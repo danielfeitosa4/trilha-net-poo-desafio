d50f3e1555565e0d01281f5df12a0226a22846d3
Daniel Feitosa <danielfeitosa.gama@outlook.com>
2024-11-08 19:19:15 -0300

    Finalizando projeto

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,19 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Numero { get; set; }
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        protected Smartphone(string numero, string modelo, string iMEI, int memoria)
         {
-            Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = iMEI;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+var celularNokia = new Nokia("1","Moto G", "12345", 268);
+celularNokia.Ligar();
+celularNokia.ReceberLigacao();
+celularNokia.InstalarAplicativo("Android");
+
+Console.WriteLine("--------------------------------------------");
+
+var celularIphone = new Iphone("2", "iPhone XR", "54321", 64);
+celularNokia.Ligar();
+celularNokia.ReceberLigacao();
+celularNokia.InstalarAplicativo("iOS");
